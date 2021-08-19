727dfd639d9d84dd66cb211188f285f5687da929
Paloma Vilchis <paloma.vilchis@gmail.com>
2021-08-18 21:41:40 -0500

    1

---
+++ main.c
@@ -0,0 +1,23 @@
+/*comentarios son para nosostros de futuro
+nombre del programa
+Autor
+Modificado por
+que hace el programa
+necesita un circuito
+colocar una imagen en el repositorio _ liga
+terminales que se utilizan, el modo
+ejemplo: gpio 5_salida*/
+
+/*bibliotecas*/
+#include <stdio.h>
+
+/*declaracion de variables con el tipo correspondiente*/
+
+/*estructura main*/
+
+int main()
+{
+    printf("Hola UAM"); 
+    return 0;
+}
+
